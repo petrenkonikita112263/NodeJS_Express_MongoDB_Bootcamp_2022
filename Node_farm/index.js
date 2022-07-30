691251ecb8d4ae63e5f0432d52b024301e7a12f1
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-30 15:31:08 +0300

    Example of reading and writing file.

--- Node_farm/index.js
+++ Node_farm/index.js
@@ -1,4 +1,8 @@
 const fsModule = require("fs")
 
-const hello = "Hello World!"
-console.log(hello)
+const inputText = fsModule.readFileSync(".\\txt\\input.txt", "utf-8")
+console.log(inputText)
+
+const sendingText = `This is what we know about the avocado: ${inputText}.\nCreated on ${Date.now()}`
+fsModule.writeFileSync(".\\txt\\output.txt", sendingText)
+console.log("File was writtent")
