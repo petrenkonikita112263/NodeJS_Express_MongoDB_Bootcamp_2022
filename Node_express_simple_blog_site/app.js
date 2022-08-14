474e60cc17731482e7b42a5a124c970a3cab3b45
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-08-14 16:12:28 +0300

    Replace inline css with external and navigate express to public folder.

--- Node_express_simple_blog_site/app.js
+++ Node_express_simple_blog_site/app.js
@@ -7,6 +7,9 @@ const app = express()
 // register view engine
 app.set("view engine", "ejs")
 
+// static files
+app.use(express.static("public"))
+
 // listen for requests
 app.listen(port)
 app.get("/", (request, response) => {
