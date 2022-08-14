4cc1a7f76fd16d05206e20c56d416526ac54aedc
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-08-14 17:10:44 +0300

    Retrieve and save blog into database.

--- Node_express_simple_blog_site/app.js
+++ Node_express_simple_blog_site/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const mongoose = require("mongoose")
+const Blog = require("./models/blog")
 const port = 2348
 
 // express app
@@ -8,7 +9,7 @@ const app = express()
 
 // conntect to MongoDB
 const dbURI =
-    "mongodb+srv://editor_blog:8Wgp9Dk4yCDVtjZi@cluster0.qmao9io.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://editor_blog:8Wgp9Dk4yCDVtjZi@cluster0.qmao9io.mongodb.net/blogSite"
 mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
@@ -23,6 +24,47 @@ app.set("view engine", "ejs")
 // static files & middlewares
 app.use(express.static("public"))
 app.use(morgan("dev"))
+
+// mongoose & mongo tests
+app.get("/add-blog", (request, response) => {
+    const blog = new Blog({
+        title: "new blog",
+        snippet: "about my new blog",
+        body: "more about my new blog",
+    })
+
+    blog.save()
+        .then((result) => {
+            response.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.get("/all-blogs", (request, response) => {
+    Blog.find()
+        .then((result) => {
+            response.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.get("/single-blog", (request, response) => {
+    Blog.findById("5ea99b49b8531f40c0fde689")
+        .then((result) => {
+            response.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.get("/", (request, response) => {
+    response.redirect("/blogs")
+})
 
 // routes
 app.get("/", (request, response) => {
