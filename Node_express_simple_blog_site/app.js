9aa949ac9201396260d10bb0bca12dfd612fbb35
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-08-15 17:11:20 +0300

    Create crud routes for blog.

--- Node_express_simple_blog_site/app.js
+++ Node_express_simple_blog_site/app.js
@@ -31,29 +31,60 @@ app.use(morgan("dev"))
 
 // routes
 app.get("/", (request, response) => {
-    const blogs = [
-        {
-            title: "Yoshi finds eggs",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-        {
-            title: "Mario finds stars",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-        {
-            title: "How to defeat bowser",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-    ]
-    response.render("index", { title: "Home", blogs })
+    response.redirect("/blogs")
 })
 
 app.get("/about", (request, response) => {
     response.render("about", { title: "About" })
 })
 
+// blog routes
 app.get("/blogs/create", (request, response) => {
     response.render("create", { title: "Create a new blog" })
+})
+
+app.get("/blogs", (request, response) => {
+    Blog.find()
+        .sort({ createdAt: -1 })
+        .then((result) => {
+            response.render("index", { blogs: result, title: "All blogs" })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.post("/blogs", (request, response) => {
+    const blog = new Blog(request.body)
+    blog.save()
+        .then((result) => {
+            response.redirect("/blogs")
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.get("/blogs/:id", (request, response) => {
+    const id = request.params.id
+    Blog.findById(id)
+        .then((result) => {
+            response.render("details", { blog: result, title: "Blog Details" })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.delete("/blogs/:id", (request, response) => {
+    const id = request.params.id
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            response.json({ redirect: "/blogs" })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 })
 
 // error page
