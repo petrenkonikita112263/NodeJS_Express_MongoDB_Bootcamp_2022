aa2268877196d2e931f709ef641637ce9a34ad9d
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-08-15 17:22:40 +0300

    Create routes for app and refactor code to mvc.

--- Node_express_simple_blog_site/app.js
+++ Node_express_simple_blog_site/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan")
 const mongoose = require("mongoose")
 const { MongoClient, ServerApiVersion } = require("mongodb")
 const Blog = require("./models/blog")
+const blogRoutes = require("./routes/blog_routes")
 const port = 2348
 
 // express app
@@ -39,53 +40,7 @@ app.get("/about", (request, response) => {
 })
 
 // blog routes
-app.get("/blogs/create", (request, response) => {
-    response.render("create", { title: "Create a new blog" })
-})
-
-app.get("/blogs", (request, response) => {
-    Blog.find()
-        .sort({ createdAt: -1 })
-        .then((result) => {
-            response.render("index", { blogs: result, title: "All blogs" })
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-app.post("/blogs", (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save()
-        .then((result) => {
-            response.redirect("/blogs")
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-app.get("/blogs/:id", (request, response) => {
-    const id = request.params.id
-    Blog.findById(id)
-        .then((result) => {
-            response.render("details", { blog: result, title: "Blog Details" })
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-app.delete("/blogs/:id", (request, response) => {
-    const id = request.params.id
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            response.json({ redirect: "/blogs" })
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
+app.use("/blogs", blogRoutes)
 
 // error page
 app.use((request, response) => {
