b0a779117edf260e86ec7106bfbfe43c392d603e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-08-24 09:02:36 +0300

    Handle POST request to add new tour.

--- Node_natours_api/app.js
+++ Node_natours_api/app.js
@@ -6,6 +6,8 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
+app.use(express.json())
+
 app.get("/api/v1/tours", (request, response) => {
     response.json({
         status: "success",
@@ -14,6 +16,29 @@ app.get("/api/v1/tours", (request, response) => {
             tours: tours,
         },
     })
+})
+
+app.post("/api/v1/tours", (request, response) => {
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign(
+        {
+            id: newId,
+        },
+        request.body
+    )
+    tours.push(newTour)
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (error) => {
+            response.json({
+                status: "success",
+                data: {
+                    tours: tours,
+                },
+            })
+        }
+    )
 })
 
 app.listen(port, () => {
