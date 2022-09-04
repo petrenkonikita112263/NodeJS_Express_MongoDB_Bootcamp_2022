c0d1d6f49e88dbabc16b88319b3336a4b5e58744
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-09-04 15:25:18 +0300

    Connect to Atlas cluster.

--- Node_natours_api/server.js
+++ Node_natours_api/server.js
@@ -2,6 +2,22 @@ const dotenv = require("dotenv")
 dotenv.config({ path: "./config.env" })
 const app = require("./app")
 const port = process.env.PORT
+const mongoose = require("mongoose")
+const dbUrl = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+)
+
+mongoose
+    .connect(dbUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then((connection) => {
+        console.log("DB connection is successful")
+    })
 
 app.listen(port, () => {
     console.log(`App running on port ... ${port} ... `)
