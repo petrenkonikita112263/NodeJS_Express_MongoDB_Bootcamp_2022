b6dbd47dabc19e245aa8774a205e5b2239a1983f
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-09-05 10:26:41 +0300

    Create document and save it to collection.

--- Node_natours_api/server.js
+++ Node_natours_api/server.js
@@ -17,6 +17,15 @@ const tourSchema = new mongoose.Schema({
     price: { type: Number, required: [true, "A tour must have the price"] },
 })
 const Tour = mongoose.model("Tour", tourSchema)
+const testTour = new Tour({
+    name: "The Forest Miker",
+    rating: 4.7,
+    price: 497,
+})
+testTour
+    .save()
+    .then((doc) => console.log(doc))
+    .catch((error) => console.log("ERROR 💥💥💥", error))
 
 mongoose
     .connect(dbUrl, {
