fc12d2cf2819748f4bbee24e4d9f3089d29107b1
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-09-10 14:59:15 +0300

    Install slugify and andd document middlware to create slug.

--- Node_natours_api/models/tour_model.js
+++ Node_natours_api/models/tour_model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const slugify = require("slugify")
 
 const tourSchema = new mongoose.Schema(
     {
@@ -88,6 +89,12 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual("durationWeeks").get(function () {
     return this.duration / 7
+})
+
+// Document middlware
+tourSchema.pre("save", function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
 })
 
 const Tour = mongoose.model("Tour", tourSchema)
