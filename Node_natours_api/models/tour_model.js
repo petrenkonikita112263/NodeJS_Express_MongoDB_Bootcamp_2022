997727b299caf213961e22b1b9b36efb1c8b44fc
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-09-11 13:49:52 +0300

    Create twp quesries middlware.

--- Node_natours_api/models/tour_model.js
+++ Node_natours_api/models/tour_model.js
@@ -97,5 +97,17 @@ tourSchema.pre("save", function (next) {
     next()
 })
 
+// query middlware
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+    this.start = Date.now()
+    next()
+})
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+    next()
+})
+
 const Tour = mongoose.model("Tour", tourSchema)
 module.exports = Tour
