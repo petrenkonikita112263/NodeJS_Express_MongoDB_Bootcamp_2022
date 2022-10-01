eb452cf64d6e67658e8e6564db4d54456e4b580f
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-10-01 11:50:00 +0300

    Handle dev and prod errors in error controller.

--- Node_natours_api/controllers/error_controllers.js
+++ Node_natours_api/controllers/error_controllers.js
@@ -1,7 +1,30 @@
+const sendErrorDev = (error, response) => {
+    response.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack,
+    })
+}
+
+const sendErrorProd = (error, response) => {
+    response.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack,
+    })
+}
+
 module.exports = (error, request, response, next) => {
     error.statusCode = error.statusCode || 500
     error.status = error.status || "Error"
     response
         .status(error.statusCode)
         .json({ status: error.status, message: error.message })
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(error, response)
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(error, response)
+    }
 }
